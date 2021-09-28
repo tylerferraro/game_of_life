b1d1fcaf15de254009a805836e3b30826aae5bcf
Tyler Ferraro <tyler.ferraro@gmail.com>
2021-09-27 20:30:17 -0700

    Add Board#neighbors with specs

--- lib/game_of_life/board.rb
+++ lib/game_of_life/board.rb
@@ -11,17 +11,50 @@ module GameOfLife
     def mutate!
       # Each cell
       @cells = cells.map do |cell|
-        neighbors = get_neighbors(cell)
+        neighbors = neighbors(cell)
         cell.mutate(neighbors)
       end
     end
 
-    def get_neighbors(cell)
-      []
+    def neighbors(cell)
+      index = cells.index(cell)
+      raise ArgumentError.new('Unable to find specified cell on board') if index.nil?
+
+      row = (index / rows)
+      col = (index % columns)
+
+      neighbor_offsets.map do |row_offset, col_offset|
+        r = row + row_offset
+        c = col + col_offset
+
+        is_inbounds?(r, c) ? cell_at(r, c) : nil
+      end.compact
+    end
+
+    def cell_at(row, column)
+      index = row * columns + column
+      @cells[index]
     end
 
     def to_a
       cells.map(&:age).each_slice(columns).to_a
+    end
+
+    private
+
+    def neighbor_offsets
+      [
+        [-1, -1], [-1, 0], [-1, 1],
+        [ 0, -1],          [ 0, 1],
+        [ 1, -1], [ 1, 0], [ 1, 1]
+      ]
+    end
+
+    def is_inbounds?(row, col)
+      row >= 0 &&
+      row < rows &&
+      col >= 0 &&
+      col < columns
     end
   end
 end
--- spec/game_of_life/board_spec.rb
+++ spec/game_of_life/board_spec.rb
@@ -29,7 +29,42 @@ RSpec.describe GameOfLife::Board do
     end
   end
 
-  describe '#get_neighbors' do
+  describe '#neighbors' do
+    it 'returns all 3 neighboring cells of a corner cell' do
+      cell = board.cell_at(0, 0)
+      neighbors = board.neighbors(cell)
+      expected_neighbors = [board.cell_at(0, 1), board.cell_at(1, 0), board.cell_at(1, 1)]
+
+      expect(neighbors).to eq(expected_neighbors)
+    end
+
+    it 'returns all 5 neighboring cells of an edge cell' do
+      cell = board.cell_at(1, 0)
+      neighbors = board.neighbors(cell)
+      expected_neighbors = [
+        board.cell_at(0, 0), board.cell_at(0, 1),
+        board.cell_at(1, 1),
+        board.cell_at(2, 0), board.cell_at(2, 1)
+      ]
+
+      expect(neighbors).to eq(expected_neighbors)
+    end
+
+    it 'returns all 8 neighboring cells of a central cell' do
+      cell = board.cell_at(1, 1)
+      neighbors = board.neighbors(cell)
+      expected_neighbors = board.cells.reject { |c| c == cell }
+
+      expect(neighbors).to eq(expected_neighbors)
+    end
+  end
+
+  describe '#cell_at' do
+    it 'returns the cell at [row, col]' do
+      expect(board.cell_at(1, 1).age).to eq(2)
+      expect(board.cell_at(2, 0).age).to eq(0)
+      expect(board.cell_at(2, 1).age).to eq(1)
+    end
   end
 
   describe '#to_a' do
